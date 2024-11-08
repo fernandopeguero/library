544426f6016529e135f104d83431507b74fa734e
Fernando Peguero <fernandopeguerofs@gmail.com>
2024-11-08 16:33:29 -0500

    Add functionality to add books to library

--- js/script.js
+++ js/script.js
@@ -1,11 +1,15 @@
 let library = [];
 
 // constructor function to create book
-function Book() {
-
+function Book(title, author, year, read = false,img = '') {
+    this.title = title;
+    this.authro = author;
+    this.year = year;
+    this.read = read;
+    this.poster = img;
 }
 
 
-function addBookToLibrary() {
-    
+function addBookToLibrary(book) {
+    library.push(new Book(book.title, book.author, book.year, book.read, book.img));
 }
